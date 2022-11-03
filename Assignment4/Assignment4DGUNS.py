632e095ed7350cc4e756080a8aca9ee4776f791d
Dan Guns <danbguns@gmail.com>
2022-11-03 13:56:51 -0300

    Final product with comments

--- Assignment4/Assignment4DGUNS.py
+++ Assignment4/Assignment4DGUNS.py
@@ -8,6 +8,61 @@ import assign4functions as sq
 #  Class: Prog 1700
 #  Database Assignment
 
-sq.main()
+def main():
+    conn = sq.create_connection(r"assignment4database.db")
+    ###This creates the connection and forms the database if it does not exist
+    sq.create_projects_table(conn)
+    ###this creates the project table if it does not exist
+    sq.create_tasks_table(conn)
+    ### this creates the tasks table if it does not exist
+    
+    userinput = 0
+    print(            "Welcome to the Project Manager!")
+    
 
-print("\nThanks for using this Project Manager!")
+    while userinput != 'q':
+        ###this will loop through the various inputs unless the user hits 'q'
+        userinput = input("\n     Press 1 see all projects: \n     Press 2 see all tasks: \n     Press 3 to create a new project: \n     Press 4 to create a new task: \n     Press 5 to delete a project: \n     Press 6 to delete a task:\n     Press q to quit:\n\nEnter your input here: ").strip()
+        ###all the descriptions for these functions can be found in assign4functions.py
+        if userinput == '1':
+            try:
+                sq.select_all_projects(conn)
+            except:
+                print("\nError: you must have entered somthing wrong!")
+        elif userinput == '2':     
+            try:
+                sq.select_all_tasks(conn)
+            except:
+                print("\nError: you must have entered somthing wrong!") 
+        elif userinput == '3':
+            try:
+                sq.create_new_project(conn)
+            except:
+                print("\nError: you must have entered somthing wrong!") 
+        elif userinput == '4':
+            try:
+                sq.create_new_task(conn)
+            except:
+                ### this is a specific error message as some of the fields need to be integers
+                print("\nError: you must have entered somthing wrong! \n Fields 1, 3, 4 need to be Integers!\n")  
+        elif userinput == '5':
+            try:
+                sq.delete_a_project(conn)
+            except:
+                print("\nError: you must have entered somthing wrong!")  
+        elif userinput == '6':
+            try:
+                sq.delete_a_task(conn)
+            except:
+                print("\nError: you must have entered somthing wrong!")
+        elif userinput == 'q':
+            break
+        else:
+            print("\nThat is not a recognized Input")
+
+    sq.close_connection()
+    ###this makes sure the connection closes after the user is finished
+
+    print("\nThanks for using this Project Manager!")
+main()
+###this calles the main logic of the program
--- Assignment4/assign4functions.py
+++ Assignment4/assign4functions.py
@@ -14,6 +14,7 @@ def create_connection(db_file):
     :param db_file:database file
     :return: Connectoion object or None
     """
+    ###this creats the database if it does nto exist
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -23,23 +24,14 @@ def create_connection(db_file):
     
     return conn
 
-def close_connection():
-    """creating a connection to SQLite database"""
-    conn = None
-    try:
-        conn = sqlite3.connect(r"assignment4database.db")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-            print("\nConnection closed sucessfully")
 
 def create_table(conn, create_table_sql):
     """create a table from the create_table_sql statement
     :param conn: Connection object
     param create_table_sql: a CREATE TABLE statement
     :return"""
+    ###This is the function to create a table in the database
+    ###Used for both creating the project and task table
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
@@ -48,24 +40,23 @@ def create_table(conn, create_table_sql):
 
 
 
-def create_projects_table():
-    database = r"assignment4database.db"
+def create_projects_table(conn):
+    ### THis creates the projects table if it doesnt exist
 
     sql_create_projects_table = """CREATE TABLE IF NOT EXISTS projects (
                                          id integer PRIMARY KEY,
                                          name text NOT NULL,                                        begin_date text,
                                          end_date text
                                         ); """
-    conn = create_connection(database)
+    
 
     if conn is not None:
         create_table(conn, sql_create_projects_table)
     else:
-        print("Error! Cannot create database connection.")
+        print("\nError! Cannot create database connection.")
 
-def create_tasks_table():
-    database = r"assignment4database.db"
-
+def create_tasks_table(conn):
+    ###This creates the task table if it doesnt exist
     sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
                                  id integer PRIMARY KEY,
                                  name text NOT NULL,
@@ -76,68 +67,70 @@ def create_tasks_table():
                                  end_date text NOT NULL,
                                  FOREIGN KEY (project_id) REFERENCES projects (id)
                              );"""
-    conn = create_connection(database)
+    
 
     if conn is not None:
         create_table(conn, sql_create_tasks_table)
     else:
-        print("Error! Cannot create database connection.")
+        print("\nError! Cannot create database connection.")
 
-def create_project(conn, project):
-    """
-    Create a new project into the projects table
-    :param conn:
-    :param project:
-    :return project id:
-    """
-    sql = '''INSERT INTO projects(name,begin_date,end_date)
-            VALUES(?,?,?)'''
+def select_all_projects(conn):
+    ###This function Selects the projects from the database to display them
     cur = conn.cursor()
-    cur.execute(sql, project)
-    conn.commit()
-    return cur.lastrowid
+    cur.execute("SELECT * FROM projects")
+
+    rows = cur.fetchall()
+    ###this takes the column values and prints them
+    data = cur.execute("""SELECT * FROM projects""")
+    for column in data.description:
+        print(column[0],  end=" - ")
+    print("")
+    for row in rows:
+        
+        print(row)
 
 
-def create_task(conn, task):
-    """
-    Create a new task
-    :param conn:
-    :param task:
-    :return:
-    """
-
-    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
-              VALUES(?,?,?,?,?,?)'''
+def select_all_tasks(conn):
+    ###this function selects the tasks and displayes them
+    
     cur = conn.cursor()
-    cur.execute(sql, task)
-    conn.commit()
+    cur.execute("SELECT * FROM tasks")
 
-    return cur.lastrowid
+    rows = cur.fetchall()
+    ### this takes the column values and prints them
+    data = cur.execute("""SELECT * FROM tasks""")
+    for column in data.description:
+        print(column[0],  end=" - ")
+    print("")
+    for row in rows:
+        print(row)
 
-
-def create_new_project():
-    database = r"assignment4database.db"
-
-    #create a database connection
-
-    conn = create_connection(database)
-
+def create_new_project(conn):
+    
+    ###this function creates new projects by taking lines of input,
+    ### and forming a list with them, and creating a tuple of the values
     with conn:
         projname = input("\nEnter the new project name: ")
         projdate1 = input("Enter the project start date(XXXX-XX-XX): ")
         projdate2 = input("Enter the project end date(XXXX-XX-XX): ")
         project = [projname, projdate1, projdate2]
         project = tuple(project)
-        # project = input("enter name date + date\n").split()
-        # project = tuple(project);
-        project_id = create_project(conn, project)
+    
+        sql = '''INSERT INTO projects(name,begin_date,end_date)
+                VALUES(?,?,?)'''
+
+        cur = conn.cursor()
+        cur.execute(sql, project)
+        conn.commit()
+        return cur.lastrowid
 
 
-def create_new_task():
-    database = r"assignment4database.db"
 
-    conn = create_connection(database)
-    select_all_projects()
+def create_new_task(conn):
+    
+    select_all_projects(conn)
+    ###This function takes inputs, forms a list with them,
+    ###Creates a tuple from the list, so it can be entered into the database
     with conn:
         
         tasknum3 = input("\nEnter project_id(int): ")
@@ -151,131 +144,55 @@ def create_new_task():
         taskchange = tuple(taskchange)
 
         
-        
-        
-        create_task(conn, taskchange)
+        sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+                VALUES(?,?,?,?,?,?)'''
+        cur = conn.cursor()
+        cur.execute(sql, taskchange)
+        conn.commit()
+
+        return cur.lastrowid
 
 
-def delete_task(conn, id):
-    """
-    Delete a task by task id
-    :param conn:
-    :param id: id of the task:
-    :return:
-    """
-    sql = 'DELETE FROM tasks WHERE id = ?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
-
-
-def delete_project(conn, id):
-
-    sql = 'DELETE FROM projects WHERE id = ?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
-
-
-def delete_a_task():
-    select_all_tasks()
-    database = r"assignment4database.db"
-
-    conn = create_connection(database)
-    taskid = input("\nEnter the task number to delete: ")
-    with conn:
-       delete_task(conn, int(taskid))#;
-
-
-def delete_a_project():
-    select_all_projects()
-    database = r"assignment4database.db"
-
-    conn = create_connection(database)
+def delete_a_project(conn):
+    select_all_projects(conn)
+    ###this function deletes a project from the list of projects
+    
     projid = input("\nEnter the project number to delete: ")
     with conn:
-       delete_project(conn, int(projid))
+       
+        sql = 'DELETE FROM projects WHERE id = ?'
+        cur = conn.cursor()
+        cur.execute(sql, (projid,))
+        conn.commit()
 
 
 
-def select_all_tasks():
+
+def delete_a_task(conn):
+    select_all_tasks(conn)
+    ### this function deletes a task from the list of tasks
     
-    conn = create_connection(r"assignment4database.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM tasks")
-
-    rows = cur.fetchall()
-
-    print("ID# - TaskName - priority - status_id - project_id - begin_date - end_date ")
-    for row in rows:
-        print(row)
+    taskid = input("\nEnter the task number to delete: ")
+    with conn:
+      
+        sql = 'DELETE FROM tasks WHERE id = ?'
+        cur = conn.cursor()
+        cur.execute(sql, (taskid,))
+        conn.commit()
 
 
-def select_all_projects():
-    conn = create_connection(r"assignment4database.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM projects")
-
-    rows = cur.fetchall()
-
-    print("ID# - projname - begin_date - end_date ")
-    for row in rows:
-        
-        print(row)
-
-def lookattheprojects():
+ 
+def close_connection():
+    ###this function closes the connection to the database
+    conn = None
     try:
-        select_all_projects()
-    except:
-        print("Error: you must have entered somthing wrong!")
-             
+        conn = sqlite3.connect(r"assignment4database.db")
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+            print("\nConnection closed sucessfully")
+  
 
-def lookathetasks():
-    try:
-        select_all_tasks()
-    except:
-        print("Error: you must have entered somthing wrong!") 
 
-def createtheproject():
-    
-    try:
-        create_new_project()
-    except:
-        print("Error: you must have entered somthing wrong!") 
-
-def createthetask():
-    try:
-        create_new_task()
-    except:
-        print("Error: you must have entered somthing wrong! \n Fields 1, 3, 4 need to be Ints!\n")  
-
-def deletetheproject():
-    try:
-        delete_a_project()
-    except:
-        print("Error: you must have entered somthing wrong!")  
-
-def deletethetask():
-    try:
-        delete_a_task()
-    except:
-        print("Error: you must have entered somthing wrong!")  
-
-def main():
-    conn = create_connection(r"assignment4database.db")
-    create_projects_table()
-    create_tasks_table()
-    listoffunctions = [select_all_projects, select_all_tasks, createtheproject, createthetask, delete_a_project, delete_a_task]
-    userinput = 0
-    print(            "Welcome to the Project Manager!\n")
-    
-
-    while userinput != 'q':
-        userinput = input("     Press 1 see all projects: \n     Press 2 see all tasks: \n     Press 3 to create a new project: \n     Press 4 to create a new task: \n     Press 5 to delete a project: \n     Press 6 to delete a task:\n     Press q to quit:" ).strip()
-        if userinput == 'q':
-            break
-        userinput = int(userinput)
-        userinput = userinput - 1
-        listoffunctions[userinput]()
-
-    close_connection()
