101c325922d833a57976ab1fb520ba876bf568dd
Dan Guns <danbguns@gmail.com>
2022-11-24 19:29:14 -0400

    Final project game

---
+++ FinalProjGame/functions.py
@@ -0,0 +1,36 @@
+import pygame as py
+import os as os
+playerposition = py.Rect(200, 450, 100, 90)
+coneposition = py.Rect(1400, 450, 150, 75)
+blenderposition = py.Rect(1400, 420, 150, 75)
+ballposition = py.Rect(1400,200, 60,60)
+pigposition = py.Rect(3000, 450, 200, 128)
+charlie_fear = 0
+
+jump = True
+jumpcount = 13
+scrollstartx = 0
+scroll = 0
+move = 1200
+gameclock = 0
+itemvel = 4
+immunetimer = 60
+boosttimer = 0
+run = True
+
+
+def control_object(itemvel, timestart, objectposition, object):
+    if gameclock >= timestart:
+        objectposition.x -= itemvel
+        screen.blit(object, (objectposition.x, objectposition.y))
+        if objectposition.x <= -2000:
+            objectposition.x = 1400
+def gameover():
+    game_over_text = END_FONT.render("GAME OVER", 1, WHITE)
+    score_text = SCORE_FONT.render("You scored " + str(gameclock//60), 1, WHITE)
+    screen.blit(game_over_text, (WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2))
+    screen.blit(score_text, (WIDTH/2 - score_text.get_width()/2, 400))
+    py.display.update()
+    with open("scores.txt", "a") as f:
+                f.write("The score by player is " + str(gameclock//60))
+    py.time.delay(5000)
